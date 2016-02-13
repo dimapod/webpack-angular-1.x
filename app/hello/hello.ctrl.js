e9020a691de81f3841521a7b320babcb86363deb
dimapod <dimapod@gmail.com>
2016-02-13 13:42:36 +0100

    Simplification

--- app/hello/hello.ctrl.js
+++ app/hello/hello.ctrl.js
@@ -3,8 +3,4 @@
 import angular from 'angular'
 
 angular.module('app.hello').controller('helloCtrl', function() {
-  require(['./Greet.js'], Greet => {
-    var greet = new Greet();
-    greet.tell();
-  });
 });
